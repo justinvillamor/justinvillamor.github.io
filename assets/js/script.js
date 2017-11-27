f0090620ecf7fdcf9db5c4fe440658624a9e2d42
Justin Villamor <jvillamor@berkeley.edu>
2017-11-26 20:48:09 -0800

    a lot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,266 @@
 $(document).ready(function() {
+  //jQuery Function Number 1
+  $(".berk").click(function() {
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+      //jQuery Function Number 2
+      $(".container").removeClass("container");
+      //jQuery Function Number 3
+      $("body").addClass("about-background-active");
+  });
+  $(".dish").click(function() {
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+      $(".container").removeClass("container");
+      $("body").addClass("dish-background-active");
+  });
+  $(".food").click(function() {
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+      $(".container").removeClass("container");
+      $("body").addClass("food-background-active");
+  });
+  $(".young").click(function() {
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+      $(".container").removeClass("container");
+      $("body").addClass("young-background-active");
+  });
+  $(".hike").click(function() {
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+      $(".container").removeClass("container");
+      $("body").addClass("hike-background-active");
+  });
+  $(".beach").click(function() {
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+      $(".container").removeClass("container");
+      $("body").addClass("beach-background-active");
+  });
+  $(".game").click(function() {
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+      $(".container").removeClass("container");
+      $("body").addClass("game-background-active");
+  });
 
+  $(".x-button").click(function() {
+    // jQuery Function Number 4
+    if ($("body").hasClass("about-background-active")) {
+      $("body").removeClass("about-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("dish-background-active")) {
+      $("body").removeClass("dish-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("food-background-active")) {
+      $("body").removeClass("food-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("young-background-active")) {
+      $("body").removeClass("young-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("hike-background-active")) {
+      $("body").removeClass("hike-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("beach-background-active")) {
+      $("body").removeClass("beach-background-active");
+      $("body").addClass("container");
+    }
+    if ($("body").hasClass("game-background-active")) {
+      $("body").removeClass("game-background-active");
+      $("body").addClass("container");
+    }
+  });
+  // jQuery Function Number 5
+  $(".x-button").hover(function() {
+    // jQuery Function Number 6
+    $(this).css("cursor", "pointer");
+  })
 });
